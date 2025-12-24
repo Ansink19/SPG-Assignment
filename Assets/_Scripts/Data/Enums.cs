77b74f4488b0c149ec9b6fc296b8c57529f64bda
Ansink <1234kukreja@gmail.com>
2025-12-25 01:14:01 +0530

    Moved Enums Out

---
+++ Assets/_Scripts/Data/Enums.cs
@@ -0,0 +1,27 @@
+
+public enum WinState
+{
+    AI,
+    Player,
+    Draw,
+    None
+}
+
+public enum GameState
+{
+    Menu,
+    Play,
+    Reveal,
+    PostGame
+}
+
+public enum GameElements
+{
+    Rock,
+    Paper,
+    Scissors,
+    Lizard,
+    Spock,
+    None
+}
+
--- Assets/_Scripts/World.cs
+++ Assets/_Scripts/World.cs
@@ -136,29 +136,3 @@ public class World : MonoBehaviour
         return WinState.AI;
     }
 }
-
-public enum WinState
-{
-    AI,
-    Player,
-    Draw,
-    None
-}
-
-public enum GameState
-{
-    Menu,
-    Play,
-    Reveal,
-    PostGame
-}
-
-public enum GameElements
-{
-    Rock,
-    Paper,
-    Scissors,
-    Lizard,
-    Spock,
-    None
-}
